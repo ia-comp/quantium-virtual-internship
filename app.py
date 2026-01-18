40590bdb7d7e206e3362d1bfee63267578ea1e52
I_A <none>
2026-01-18 23:45:20 +1100

    task4 - add styling complete

--- app.py
+++ app.py
@@ -2,17 +2,22 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
 CSV_FILE = 'output.csv'
+region_colour_map = {
+    "north": "royalblue",
+    "south": "firebrick",
+    "east": "forestgreen",
+    "west": "purple"
+}
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash()
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv(CSV_FILE, parse_dates=['Date'])
-
-fig = px.line(df, x="Date", y="Sales", title='Pink Morsel Sales')
 
 app.layout = html.Div(children=[
     html.H1(children='Soul Origin Dashboard'),
@@ -23,9 +28,34 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='pink-morsels-sales',
-        figure=fig
+    ),
+
+    dcc.RadioItems(
+        id='region-radio',
+        options=df.Region.unique(),
+        value=df.Region.unique()[0]
     )
+
 ])
+
+@app.callback(
+    Output('pink-morsels-sales', 'figure'),
+    Input('region-radio', 'value')
+)
+def update_graph(selected_region):
+    # Filter data based on radio selection
+    filtered_df = df[df['Region'] == selected_region]
+    
+    # Create line graph with only the selected data
+    fig = px.line(
+        filtered_df, 
+        x='Date', 
+        y='Sales', 
+        color='Region', 
+        color_discrete_map=region_colour_map, 
+        title=f'Pink Morsel Sales'
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
